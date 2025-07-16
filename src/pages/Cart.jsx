80ea26acc727400088953ff31461713d63391d85
Richard Nixon <rnixonaf@gmail.com>
2025-07-16 23:25:09 +0100

    fix update quantity itens and add button + and  -

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import "../Cart.css";
 
 export default function Cart() {
   const [cartItems, setCartItems] = useState([]);
@@ -16,28 +17,60 @@ export default function Cart() {
     setCartItems(updatedCart);
   };
 
+  const updateQuantity = (id, delta) => {
+    const updatedCart = cartItems.map((item) => {
+     if (item.product._id === id) {
+        const newQuantity = item.quantity + delta;
+        return { ...item, quantity: newQuantity > 0 ? newQuantity : 1 };
+     }
+      return item;
+   });
+    setCartItems(updatedCart);
+    localStorage.setItem("cart", JSON.stringify(updatedCart));
+  };
+
+
+
   const total = cartItems.reduce(
     (acc, item) => acc + item.product.price * item.quantity,
     0
   );
 
   return (
-    <div>
-      <h1>Shopping Cart</h1>
+    <div className="cart-container">
+      <h1>Seu Carrinho</h1>
       {cartItems.length === 0 ? (
         <p>Your cart is empty.</p>
       ) : (
-        <div>
+        <>
           {cartItems.map(({ product, quantity }) => (
-            <div key={product._id}>
-              <h2>{product.name}</h2>
-              <p>€ {product.price.toFixed(2)} × {quantity}</p>
-              <button onClick={() => removeItem(product._id)}>Remove</button>
+            <div className="cart-item" key={product._id}>
+              <img
+                src={`http://localhost:5005${product.imageUrl}`}
+                alt={product.name}
+                className="product-image"
+              />
+              <div className="item-info">
+                <h2>{product.name}</h2>
+                <p>
+                    R$ {product.price.toFixed(2)} × {quantity}
+                    <button onClick={() => updateQuantity(product._id, -1)}>-</button>
+                    <button onClick={() => updateQuantity(product._id, 1)}>+</button>
+                </p>
+              </div>
+              <button
+                className="remove-button"
+                onClick={() => removeItem(product._id)}
+              >
+                Remover
+              </button>
             </div>
           ))}
-          <h3>Total: € {total.toFixed(2)}</h3>
-          <button onClick={() => navigate("/checkout")}>Go to Checkout</button>
-        </div>
+          <h3>Total: R$ {total.toFixed(2)}</h3>
+          <button className="checkout-button" onClick={() => navigate("/checkout")}>
+            Finalizar Ordem de Compra
+          </button>
+        </>
       )}
     </div>
   );
