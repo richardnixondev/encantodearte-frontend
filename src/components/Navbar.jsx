57da1df7fa9d999f77e1488b3d1c6d6587de175b
Richard Nixon <rnixonaf@gmail.com>
2025-07-16 23:50:53 +0100

    add badger with num,ber of items at cart

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,27 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { Link } from "react-router-dom";
 import { FiShoppingCart, FiUser } from "react-icons/fi";
-import "/src/Navbar.css";
+import "../Navbar.css";
 
 export default function Navbar() {
   const { user } = useContext(AuthContext);
+  const [cartCount, setCartCount] = useState(0);
+
+  useEffect(() => {
+    const updateCartCount = () => {
+      const cart = JSON.parse(localStorage.getItem("cart")) || [];
+      const total = cart.reduce((acc, item) => acc + item.quantity, 0);
+      setCartCount(total);
+    };
+
+    updateCartCount(); // inicial
+    window.addEventListener("cartUpdated", updateCartCount);
+
+    return () => {
+      window.removeEventListener("cartUpdated", updateCartCount);
+    };
+  }, []);
 
   return (
     <nav className="navbar">
@@ -19,13 +35,16 @@ export default function Navbar() {
       </div>
 
       <div className="navbar-icons">
-        <Link to={user ? "/profile" : "/login"} className="navbar-icon">
+        <Link to="/" className="navbar-icon">
           <FiUser size={22} />
           <span>{user ? user.surname : "Login"}</span>
         </Link>
 
-        <Link to="/cart" className="navbar-icon">
+        <Link to="/cart" className="navbar-icon cart-icon">
           <FiShoppingCart size={22} />
+          {cartCount > 0 && (
+            <span className="cart-count">{cartCount}</span>
+          )}
         </Link>
       </div>
     </nav>
